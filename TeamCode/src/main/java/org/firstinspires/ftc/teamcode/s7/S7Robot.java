3e618cac5d873a0692b3967979bd5bb96cb7f968
Jack Jones <jackij43@outlook.com>
2023-08-16 17:06:17 -0500

    add S7Robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/S7Robot.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode.s7;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Line;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class S7Robot {
+    public SampleMecanumDrive drive;
+
+    private LinearOpMode opMode;
+
+    public S7Robot(LinearOpMode opMode) {
+        this.opMode = opMode;
+        this.drive = new SampleMecanumDrive(opMode.hardwareMap);
+    }
+
+    public S7Robot(LinearOpMode opMode, Pose2d startPose) {
+        this.opMode = opMode;
+        this.drive = new SampleMecanumDrive(opMode.hardwareMap);
+        this.drive.setPoseEstimate(startPose);
+    }
+
+    //drive functions
+    public void setWeightedDrivePower(Pose2d drivePower) {
+        drive.setWeightedDrivePower(drivePower);
+    }
+
+    public void lineTo(Vector2d endPosition) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .lineTo(endPosition)
+                .build());
+    }
+
+    public void lineToConstantHeading(Vector2d endPosition) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .lineToConstantHeading(endPosition)
+                .build());
+    }
+
+    public void lineToLinearHeading(Pose2d endPose) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .lineToLinearHeading(endPose)
+                .build());
+    }
+
+    public void lineToSplineHeading(Pose2d endPose) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .lineToSplineHeading(endPose)
+                .build());
+    }
+
+    public void strafeTo(Vector2d endPosition) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .strafeTo(endPosition)
+                .build());
+    }
+
+    public void forward(double distance) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .forward(distance)
+                .build());
+    }
+
+    public void back(double distance) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .back(distance)
+                .build());
+    }
+
+    public void strafeLeft(double distance) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .strafeLeft(distance)
+                .build());
+    }
+
+    public void strafeRight(double distance) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .strafeRight(distance)
+                .build());
+    }
+
+    public void splineTo(Vector2d endPosition, double endTangent) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .splineTo(endPosition, endTangent)
+                .build());
+    }
+
+    public void splineToConstantHeading(Vector2d endPosition, double endTangent) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .splineToConstantHeading(endPosition, endTangent)
+                .build());
+    }
+
+    public void splineToLinearHeading(Pose2d endPose, double endTangent) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .splineToLinearHeading(endPose, endTangent)
+                .build());
+    }
+
+    public void splineToSplineHeading(Pose2d endPose, double endTangent) {
+        followTrajectoryAndWait(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .splineToSplineHeading(endPose, endTangent)
+                .build());
+    }
+
+    //TODO: add overrides for different max velocity and accel
+
+    public void followTrajectoryAndWait(Trajectory trajectory) {
+        drive.followTrajectory(trajectory);
+        waitForDrive();
+    }
+
+    //Meta
+    public void waitForDrive() {
+        drive.waitForIdle();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/opmode/TestS7RobotAuto.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.s7.opmode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.s7.S7Robot;
+
+@Config
+@TeleOp(name="S7Robot test")
+public class TestS7RobotAuto extends LinearOpMode {
+    public static double DISTANCE = 48;
+
+    private S7Robot robot;
+
+    @Override
+    public void runOpMode() {
+        robot = new S7Robot(this, new Pose2d(-61.01, -61.39, Math.toRadians(90.00)));
+
+        waitForStart();
+
+        /*
+        Export from RRPathGen
+
+        TrajectorySequence untitled0 = drive.trajectorySequenceBuilder(new Pose2d(-61.01, -61.39, Math.toRadians(90.00)))
+        .lineToConstantHeading(new Vector2d(-35.81, -35.81))
+        .lineToConstantHeading(new Vector2d(-59.51, -12.58))
+        .splineTo(new Vector2d(-47.21, 6.53), Math.toRadians(0.00))
+        .splineTo(new Vector2d(-34.91, -5.77), Math.toRadians(-90.00))
+        .build();
+        */
+
+        robot.lineToConstantHeading(new Vector2d(-35.81, -35.81));
+        robot.lineToConstantHeading(new Vector2d(-59.51, -12.58));
+        robot.splineTo(new Vector2d(-47.21, 6.53), Math.toRadians(0.00));
+        robot.splineTo(new Vector2d(-34.91, -5.77), Math.toRadians(-90.00));
+    }
+}
