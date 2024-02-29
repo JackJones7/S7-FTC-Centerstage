5db07e4de73335b6b7ee3f7697d2bc3550dc25a2
Jack Jones <jackij43@outlook.com>
2024-02-29 17:56:43 -0600

    Add S7Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/S7Drive.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode.s7;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.trajectory.config.TrajectoryConfigManager;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
+
+import java.io.File;
+
+public class S7Drive {
+
+    public SampleMecanumDrive drive;
+
+
+    private TrajectoryVelocityConstraint VEL_CONSTRAINT;
+    private TrajectoryAccelerationConstraint ACCEL_CONSTRAINT;
+
+
+    public S7Drive(HardwareMap hardwareMap) {
+        this.drive = new SampleMecanumDrive(hardwareMap);
+        this.VEL_CONSTRAINT = drive.getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
+        this.ACCEL_CONSTRAINT = drive.getAccelerationConstraint(MAX_ACCEL);
+    }
+
+    public S7Drive(HardwareMap hardwareMap, double maxVel, double maxAngVel, double maxAccel) {
+        this.drive = new SampleMecanumDrive(hardwareMap);
+        this.VEL_CONSTRAINT = drive.getVelocityConstraint(maxVel, maxAngVel, TRACK_WIDTH);
+        this.ACCEL_CONSTRAINT = drive.getAccelerationConstraint(maxAccel);
+    }
+
+
+    public void followTrajectory(Trajectory trajectory) {
+        drive.followTrajectory(trajectory);
+    }
+
+    public void followTrajectorySequence(TrajectorySequence sequence) {
+        drive.followTrajectorySequence(sequence);
+    }
+
+    public Trajectory loadTrajectory(String filename) {
+        File file = new File(filename);
+        return TrajectoryConfigManager.load(file);
+    }
+
+
+    public void setVelConstraint(double maxVel, double maxAngVel) {
+        VEL_CONSTRAINT = drive.getVelocityConstraint(maxVel, maxAngVel, TRACK_WIDTH);
+    }
+
+    public void setAccelConstraint(double maxAccel) {
+        ACCEL_CONSTRAINT = drive.getAccelerationConstraint(maxAccel);
+    }
+
+
+    private TrajectoryBuilder trajectoryBuilder(Pose2d startPose, boolean reversed) {
+        return new TrajectoryBuilder(startPose, reversed, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+
+    public Trajectory lineTo(Vector2d endPosition, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).lineTo(endPosition).build();
+    }
+
+    public Trajectory lineToConstantHeading(Vector2d endPosition, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).lineToConstantHeading(endPosition).build();
+    }
+
+    public Trajectory lineToLinearHeading(Pose2d endPose, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).lineToLinearHeading(endPose).build();
+    }
+
+    public Trajectory lineToSplineHeading(Pose2d endPose, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).lineToSplineHeading(endPose).build();
+    }
+
+    public Trajectory splineTo(Vector2d endPosition, double endTangent, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).splineTo(endPosition, endTangent).build();
+    }
+
+    public Trajectory splineToConstantHeading(Vector2d endPosition, double endTangent, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).splineToConstantHeading(endPosition, endTangent).build();
+    }
+
+    public Trajectory splineToLinearHeading(Pose2d endPose, double endTangent, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).splineToLinearHeading(endPose, endTangent).build();
+    }
+
+    public Trajectory splineToSplineHeading(Pose2d endPose, double endTangent, boolean reversed) {
+        return trajectoryBuilder(drive.getPoseEstimate(), reversed).splineToSplineHeading(endPose, endTangent).build();
+    }
+
+}
