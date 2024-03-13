4303b8e082ede8fa2e4b2de659e3003d849ae507
Jack Jones <jackij43@outlook.com>
2024-03-13 17:55:30 -0600

    add arm control functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/S7ArmAssembly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/S7ArmAssembly.java
@@ -10,11 +10,11 @@ public class S7ArmAssembly {
     private Servo clawServoLeft;
     private Servo clawServoRight;
 
-    private final double ARM_PIVOT_WEIGHT = 1 / 300;
-    private final double WRIST_PIVOT_WEIGHT = 1 / 300;
-    private double currentArmPivotWeight = ARM_PIVOT_WEIGHT;
-    private double currentWristPivotWeight = WRIST_PIVOT_WEIGHT;
+    private final double ARM_PIVOT_WEIGHT = 1.0 / 300.0;
+    private final double WRIST_PIVOT_WEIGHT = 1.0 / 300.0;
 
+    private double armPivotMinDegrees = 0;
+    private double armPivotMaxDegrees = 0;
     private double wristPivotMinDegrees = 0;
     private double wristPivotMaxDegrees = 300;
 
@@ -31,19 +31,35 @@ public class S7ArmAssembly {
         wristPivotServo = hardwareMap.get(Servo.class, "LowerPivot");
         clawServoLeft = hardwareMap.get(Servo.class, "LeftClaw");
         clawServoRight = hardwareMap.get(Servo.class, "RightClaw");
+
+        wristPivotServo.setDirection(Servo.Direction.REVERSE);
+        armPivotServoRight.setDirection(Servo.Direction.REVERSE);
+        clawServoRight.setDirection(Servo.Direction.REVERSE);
+    }
+
+    private static double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
     }
 
 
+    public void setArmConstraints(double minDegrees, double maxDegrees) {
+        armPivotMinDegrees = minDegrees;
+        armPivotMaxDegrees = maxDegrees;
+    }
+
+    public void setArmAngle(double degrees) {
+        armPivotServoLeft.setPosition(clamp(degrees, armPivotMinDegrees, armPivotMaxDegrees) * ARM_PIVOT_WEIGHT);
+        armPivotServoRight.setPosition(clamp(degrees, armPivotMinDegrees, armPivotMaxDegrees) * ARM_PIVOT_WEIGHT);
+    }
+
     public void setWristConstraints(double minDegrees, double maxDegrees) {
-        wristPivotServo.scaleRange(minDegrees * WRIST_PIVOT_WEIGHT, maxDegrees * WRIST_PIVOT_WEIGHT);
-        currentWristPivotWeight = 1 / (maxDegrees - minDegrees);
         wristPivotMinDegrees = minDegrees;
         wristPivotMaxDegrees = maxDegrees;
     }
 
-    public void setWristAngleAbsolute(double degrees) {
+    public void setWristAngleRelative(double degrees) {
         wristAngleType = AngleType.ABSOLUTE;
-        wristPivotServo.setPosition(degrees * currentWristPivotWeight);
+        wristPivotServo.setPosition(clamp(degrees, wristPivotMinDegrees, wristPivotMaxDegrees) * WRIST_PIVOT_WEIGHT);
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/opmode/TestS7ArmAssembly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/opmode/TestS7ArmAssembly.java
@@ -10,7 +10,13 @@ import org.firstinspires.ftc.teamcode.s7.S7Robot;
 @Autonomous (name = "TEST-S7 Arm Assembly")
 public class TestS7ArmAssembly extends LinearOpMode {
 
-    public static double ANGLE = 150;
+    public static double WRIST_ANGLE = 145;
+    public static double WRIST_MIN_ANGLE = 145;
+    public static double WRIST_MAX_ANGLE = 300;
+
+    public static double ARM_ANGLE = 140;
+    public static double ARM_MIN_ANGLE = 140;
+    public static double ARM_MAX_ANGLE = 180;
 
     private S7Robot robot;
 
@@ -20,7 +26,11 @@ public class TestS7ArmAssembly extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
-            robot.s7ArmAssembly.setWristAngleAbsolute(ANGLE);
+            robot.s7ArmAssembly.setWristConstraints(WRIST_MIN_ANGLE, WRIST_MAX_ANGLE);
+            robot.s7ArmAssembly.setWristAngleRelative(WRIST_ANGLE);
+
+            robot.s7ArmAssembly.setArmConstraints(ARM_MIN_ANGLE, ARM_MAX_ANGLE);
+            robot.s7ArmAssembly.setArmAngle(ARM_ANGLE);
         }
     }
 
